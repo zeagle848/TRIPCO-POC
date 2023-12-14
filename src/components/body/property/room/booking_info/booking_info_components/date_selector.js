e791ab68b113330d3d10ebddea4d5831af7074a5
William <31068439+zeagle848@users.noreply.github.com>
2023-12-14 15:07:17 +0200

    Created to and from date selector

--- src/components/body/property/room/booking_info/booking_info_components/date_selector.js
+++ src/components/body/property/room/booking_info/booking_info_components/date_selector.js
@@ -1,16 +1,27 @@
 import { DatePicker } from '@mui/x-date-pickers';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useState } from 'react';
+import dayjs from 'dayjs';
 
 export function DateSelector(){
+    const [minDate, setMinDate] = useState(dayjs());
+    const [maxDate, setMaxDate] = useState(null);
+    
     return(
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <div>
-                <div>
-                    <DatePicker />
+            <div className='date-selector-container'>
+                <div className='date-selector-element'>
+                    <h3>
+                        To Date:
+                    </h3>
+                    <DatePicker disablePast maxDate={maxDate || undefined} onChange={(newValue) => setMinDate(newValue)}/>
                 </div>
-                <div>
-                    <DatePicker />
+                <div className='date-selector-element'>
+                    <h3>
+                        From Date:
+                    </h3>
+                    <DatePicker disablePast minDate={minDate} onChange={(newValue) => setMaxDate(newValue)}/>
                 </div>
             </div>
         </LocalizationProvider>
