a9911f95bffe1aa9c2c03074fe3da7fdeb3db1a9
William <31068439+zeagle848@users.noreply.github.com>
2023-12-24 14:45:59 +0200

    Fixed occupancy selector

--- src/components/body/property/room/booking_info/booking_info_components/occupancy_selector.js
+++ src/components/body/property/room/booking_info/booking_info_components/occupancy_selector.js
@@ -2,6 +2,7 @@ import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import { styled } from '@mui/system';
 import { convertIntToWord } from '../../../../../../helpers/int_to_word';
+import { useState } from 'react';
 
 const StyledSelect = styled(Select)({
     width: 'index+130px',
@@ -10,14 +11,16 @@ const StyledSelect = styled(Select)({
   });
 
 export function OccupancySelector({capacity}){
+    const [selectedOccupancy, setSelectedOccupancy] = useState(1)
     return(
         <div className='occupancy-container'>
             <h5>Occupancy:</h5>
             <StyledSelect
             label="Occupancy"
             className='occupancy-selector'
-            value={1}
+            value={selectedOccupancy}
             displayEmpty
+            onChange={(newValue) => setSelectedOccupancy(newValue.target.value)}
             >
                 {Array(capacity).fill().map((_, index) => (
                     <MenuItem value={index+1}>{convertIntToWord(index+1)}</MenuItem>
